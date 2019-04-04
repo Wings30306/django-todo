0c4fb305907d4c50c2f0fe91bedb8202992372b4
Joke Heyndels <jo_hannah@outlook.com>
2019-04-04 22:49:59 +0200

    formatted templates and python files

--- todo/test_app.py
+++ todo/test_app.py
@@ -2,7 +2,8 @@ from django.apps import apps
 from django.test import TestCase
 from .apps import TodoConfig
 
+
 class TestTodoConfig(TestCase):
-  def test_app(self):
-    self.assertEqual(TodoConfig.name, "todo")
-    self.assertEqual(apps.get_app_config("todo").name, "todo")
+    def test_app(self):
+        self.assertEqual(TodoConfig.name, "todo")
+        self.assertEqual(apps.get_app_config("todo").name, "todo")
